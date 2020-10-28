757fcb8d153d535121be1958d9229406233ff962
Yuliia Savdeieva <yuliia.savdeieva@Yuliias-MacBook-Pro.local>
2020-10-28 17:59:32 +0200

    new pics and styles for them

--- components/home/WhyChoose.js
+++ components/home/WhyChoose.js
@@ -10,37 +10,38 @@ class WhyChoose extends Component {
                     <div className="section-title">
                         <h2>Fraktal Leads DeFi</h2>
                         <p>Every aspect of fractal is designed with DeFi in mind</p>
-                        <img src={require('../../static/images/logo-white.png')}/>
+                        <img className="whyus-logo" src={require('../../static/images/logo-white.png')}/>
                     </div>
                     
 
                     <div className="row-grid why-choose-grid">
                         <div className="single-features">
                             <div className="single-features-img">
+                                <img src={require('../../static/images/home/4.png')}/>
                             </div>
                             <div className="single-features__content">
                                 <h3>Infinitely Scalable</h3>
                                 <p>
-                                Each Fraktal based chain can handle 10,000 transactions per second and an infinite number of chains can run in parallel, making Fraktal the most scalable and powerful Blockchain to exist.
+                                    Each Fraktal based chain can handle 10,000 transactions per second and an infinite number of chains can run in parallel, making Fraktal the most scalable and powerful Blockchain to exist.
                                 </p>
                             </div>
                         </div>
 
                         <div className="single-features">
                             <div className="single-features-img">
-                            
+                                <img src={require('../../static/images/home/5.png')}/>
                             </div>
                             <div className="single-features__content">
                                 <h3>DeFi Optimised UX</h3>
                                 <p>
-                                From low transaction fees, zk-snarks, to near-instant transaction finality, every aspect of Fraktal is designed  with DeFi in mind.
+                                    From low transaction fees, zk-snarks, to near-instant transaction finality, every aspect of Fraktal is designed  with DeFi in mind.
                                 </p>
                             </div>
                         </div>
 
                         <div className="single-features">
                             <div className="single-features-img">
-                            
+                                <img src={require('../../static/images/home/6.png')}/>
                             </div>
                             <div className="single-features__content">
                                 <h3>Low Fees</h3>
